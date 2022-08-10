0bf94005403451aa3bef81ab0d7a464b3ef2273b
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-08-10 16:33:16 +0300

    Added the bakery items as an array of objects

---
+++ Angella's shop app/js/inventory.js
@@ -0,0 +1,33 @@
+// This barkery inventory is going to be an array of Objects.
+let bakeryInventory = [
+    {
+        name: "Bread",
+        price: 5000,
+        image:"../media/image/Bread.png"
+    },
+    {
+        name:"Cakes",
+        price:65000,
+        image:"../media/image/Cakes.png"
+    },
+    {
+        name:"Cookies",
+        price:1200,
+        image:"../media/image/Cookies.png"
+    },
+
+    {
+        name:"Hot Loaf",
+        price:35000,
+        image:"../media/image/Hot Loaf.png"
+    },
+
+    {
+        name:"Supa Loaf",
+        price:55000,
+        image:"..media/image/Supa Loaf.png"
+    },
+]
+
+
+export {bakeryInventory}
