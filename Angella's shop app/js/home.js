c5bc7451a4ec3aefcb54d148bf48acb353318bba
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-08-10 18:43:53 +0300

    Added the an onclick listener to the different buttons created dynamically

--- Angella's shop app/js/home.js
+++ Angella's shop app/js/home.js
@@ -1,45 +1,73 @@
 import { inventory } from "./inventory.js";
 
-// console.log(inventory)
-// call the container here 
-let container = document.querySelector(".container")
-// Adding the content to the page dynamica
-for(let inventoryType in inventory){
-    let itemSection = document.createElement("div")
-    itemSection.classList.add('items-section')
-    for( let items of inventory[inventoryType]){
-        let itemdiv = document.createElement("div")
-        itemdiv.classList.add('item-div')
-        let finaldiv = document.createElement("div")
-        let img = document.createElement("img")
-        img.setAttribute('src', items.image)
-        img.setAttribute('alt',`${items.name} image`)
-        // console.log(items.image)
-        img.classList.add('item-image')
-        let h4 = document.createElement('h4')
-        h4.textContent = items.name
-        let p = document.createElement('p')
-        p.textContent = items.price
-        let button = document.createElement("button")
-        button.classList.add('add-to-cart-button')
+/**
+ * @addItemsToHomePage
+ * It adds items dynamically from the inventory database. 
+ * My inventory database is an object.
+ */
+let addItemsToHomePage = ()=>{
+    // call the container here 
+    let container = document.querySelector(".container")
+    // Adding the content to the page dynamica
+    for(let inventoryType in inventory){
+        let itemSection = document.createElement("div")
+        itemSection.classList.add('items-section')
+        for( let items of inventory[inventoryType]){
+            let itemdiv = document.createElement("div")
+            itemdiv.classList.add('item-div')
+            let finaldiv = document.createElement("div")
+            let img = document.createElement("img")
+            img.setAttribute('src', items.image)
+            img.setAttribute('alt',`${items.name} image`)
+            // console.log(items.image)
+            img.classList.add('item-image')
+            let h4 = document.createElement('h4')
+            h4.textContent = items.name
+            let p = document.createElement('p')
+            p.textContent = items.price
+            let button = document.createElement("button")
+            button.textContent = "Add to cart"
+            button.classList.add('add-to-cart-button')
+            finaldiv.appendChild(img)
+            finaldiv.appendChild(h4)
+            finaldiv.appendChild(p)
+            finaldiv.appendChild(button)
 
-        finaldiv.appendChild(img)
-        finaldiv.appendChild(h4)
-        finaldiv.appendChild(p)
-        finaldiv.appendChild(button)
+            // Now append the final div to the itemsdiv
+            itemdiv.appendChild(finaldiv)
 
-        // Now append the final div to the itemsdiv
-        itemdiv.appendChild(finaldiv)
+            // Now append it to the itemSection
+            itemSection.appendChild(itemdiv)
+            // Now append the itemSection to the container.
+            container.appendChild(itemSection)
+            // console.log(`${items.name} belongs to ${inventoryType}`)
 
-        // Now append it to the itemSection
-        itemSection.appendChild(itemdiv)
-        console.log(itemSection)
-        container.appendChild(itemSection)
-        // console.log(`${items.name} belongs to ${inventoryType}`)
-
+        }
+        
     }
-    
 }
+
+// Call the addItemsToHomePage below to be able to load items to the page
+addItemsToHomePage()
+
+
 /**
  * making the 'add to cart' clickable and adding items to the cart as well.
  */
+let sup = document.querySelector(".cart-counter")
+let counter = 1
+let button = document.getElementsByClassName("add-to-cart-button")
+// let tn = document.querySelectorAll(".add-to-cart-button")
+for(let btn of button){
+    btn.addEventListener('click', (event)=>{
+        // Store what is clicked to the cookie Storage
+        
+        let itemName = btn.previousSibling.previousSibling.textContent
+        let itemImage = btn.previousSibling.previousSibling.previousSibling
+        let itemPrice = btn.previousSibling.textContent
+        console.log(itemName)
+        console.log(itemPrice)
+        sup.textContent = counter++
+        sup.style.color = "red"
+    })
+}
