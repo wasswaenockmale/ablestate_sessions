ec006eb7ed1f3820fa432dfe1db18a722e21d582
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-08-10 16:56:29 +0300

    added the inventory for clothings, footwear and electronics

--- Angella's shop app/js/inventory.js
+++ Angella's shop app/js/inventory.js
@@ -29,5 +29,95 @@ let bakeryInventory = [
     },
 ]
 
+let electronicsInventory = [
+    {
+        name: "Laptop",
+        price: 235000,
+        image:"../media/image/lattop.png"
+    },
+    {
+        name:"Watch",
+        price:135000,
+        image:"../media/image/watch.png"
+    },
+    {
+        name:"Samsung Galaxy",
+        price:335000,
+        image:"../media/image/phone.png"
+    },
 
-export {bakeryInventory}
+    {
+        name:"Electric Kettle",
+        price:5000,
+        image:"../media/image/Electric Kettle.png"
+    },
+
+    {
+        name:"Headphones",
+        price:15000,
+        image:"..media/image/headphones.png"
+    },
+]
+
+let clothingInventory = [
+    {
+        name: "Red Floral Dress",
+        price: 35000,
+        image:"../media/image/dress.png"
+    },
+    {
+        name:"Jean Shorts",
+        price:65000,
+        image:"../media/image/short.png"
+    },
+    {
+        name:"Pink Trouser women",
+        price:35000,
+        image:"../media/image/pants.png"
+    },
+
+    {
+        name:"Suit",
+        price:225000,
+        image:"../media/image/suit.png"
+    },
+
+    {
+        name:"T-shirt",
+        price:55000,
+        image:"..media/image/t-shirts.png"
+    },
+]
+
+let footwearInventory = [
+    {
+        name: "Leather Shoes",
+        price: 95250,
+        image:"../media/image/Leather shoes.png"
+    },
+    {
+        name:"Women's shoes",
+        price:95000,
+        image:"../media/image/Women's shoes.png"
+    },
+    {
+        name:"Nick Shoes",
+        price:5000,
+        image:"../media/image/nick-shoes.png"
+    },
+
+    {
+        name:"Shoes",
+        price:35000,
+        image:"../media/image/shoes.png"
+    },
+
+    {
+        name:"Sneakers",
+        price:125000,
+        image:"..media/image/Sneakers.png"
+    },
+]
+
+
+export {bakeryInventory, electronicsInventory, clothingInventory, footwearInventory}
