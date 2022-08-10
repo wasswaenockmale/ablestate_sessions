1c7409d98671f9563cd1fa89f76c54f9827a9800
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-08-10 17:11:48 +0300

    Created one item in inventory.js file that contains all the different inventory items of different types

--- Angella's shop app/js/inventory.js
+++ Angella's shop app/js/inventory.js
@@ -1,123 +1,125 @@
 // This barkery inventory is going to be an array of Objects.
-let bakeryInventory = [
-    {
-        name: "Bread",
-        price: 5000,
-        image:"../media/image/Bread.png"
-    },
-    {
-        name:"Cakes",
-        price:65000,
-        image:"../media/image/Cakes.png"
-    },
-    {
-        name:"Cookies",
-        price:1200,
-        image:"../media/image/Cookies.png"
-    },
 
-    {
-        name:"Hot Loaf",
-        price:35000,
-        image:"../media/image/Hot Loaf.png"
-    },
-
-    {
-        name:"Supa Loaf",
-        price:55000,
-        image:"..media/image/Supa Loaf.png"
-    },
-]
-
-let electronicsInventory = [
-    {
-        name: "Laptop",
-        price: 235000,
-        image:"../media/image/lattop.png"
-    },
-    {
-        name:"Watch",
-        price:135000,
-        image:"../media/image/watch.png"
-    },
-    {
-        name:"Samsung Galaxy",
-        price:335000,
-        image:"../media/image/phone.png"
-    },
-
-    {
-        name:"Electric Kettle",
-        price:5000,
-        image:"../media/image/Electric Kettle.png"
-    },
-
-    {
-        name:"Headphones",
-        price:15000,
-        image:"..media/image/headphones.png"
-    },
-]
-
-let clothingInventory = [
-    {
-        name: "Red Floral Dress",
-        price: 35000,
-        image:"../media/image/dress.png"
-    },
-    {
-        name:"Jean Shorts",
-        price:65000,
-        image:"../media/image/short.png"
-    },
-    {
-        name:"Pink Trouser women",
-        price:35000,
-        image:"../media/image/pants.png"
-    },
-
-    {
-        name:"Suit",
-        price:225000,
-        image:"../media/image/suit.png"
-    },
-
-    {
-        name:"T-shirt",
-        price:55000,
-        image:"..media/image/t-shirts.png"
-    },
-]
-
-let footwearInventory = [
-    {
-        name: "Leather Shoes",
-        price: 95250,
-        image:"../media/image/Leather shoes.png"
-    },
-    {
-        name:"Women's shoes",
-        price:95000,
-        image:"../media/image/Women's shoes.png"
-    },
-    {
-        name:"Nick Shoes",
-        price:5000,
-        image:"../media/image/nick-shoes.png"
-    },
-
-    {
-        name:"Shoes",
-        price:35000,
-        image:"../media/image/shoes.png"
-    },
-
-    {
-        name:"Sneakers",
-        price:125000,
-        image:"..media/image/Sneakers.png"
-    },
-]
-
-
-export {bakeryInventory, electronicsInventory, clothingInventory, footwearInventory}
+let inventory = {
+    bakeryInventory: [
+        {
+            name: "Bread",
+            price: 5000,
+            image:"../media/image/Bread.png"
+        },
+        {
+            name:"Cakes",
+            price:65000,
+            image:"../media/image/Cakes.png"
+        },
+        {
+            name:"Cookies",
+            price:1200,
+            image:"../media/image/Cookies.png"
+        },
+    
+        {
+            name:"Hot Loaf",
+            price:35000,
+            image:"../media/image/Hot Loaf.png"
+        },
+    
+        {
+            name:"Supa Loaf",
+            price:55000,
+            image:"..media/image/Supa Loaf.png"
+        },
+    ],
+    // Electronics inventory starts here
+    electronicsInventory: [
+        {
+            name: "Laptop",
+            price: 235000,
+            image:"../media/image/lattop.png"
+        },
+        {
+            name:"Watch",
+            price:135000,
+            image:"../media/image/watch.png"
+        },
+        {
+            name:"Samsung Galaxy",
+            price:335000,
+            image:"../media/image/phone.png"
+        },
+    
+        {
+            name:"Electric Kettle",
+            price:5000,
+            image:"../media/image/Electric Kettle.png"
+        },
+    
+        {
+            name:"Headphones",
+            price:15000,
+            image:"..media/image/headphones.png"
+        },
+    ],
+    // The clothing inventory start here
+    clothingInventory:[
+        {
+            name: "Red Floral Dress",
+            price: 35000,
+            image:"../media/image/dress.png"
+        },
+        {
+            name:"Jean Shorts",
+            price:65000,
+            image:"../media/image/short.png"
+        },
+        {
+            name:"Pink Trouser women",
+            price:35000,
+            image:"../media/image/pants.png"
+        },
+    
+        {
+            name:"Suit",
+            price:225000,
+            image:"../media/image/suit.png"
+        },
+    
+        {
+            name:"T-shirt",
+            price:55000,
+            image:"..media/image/t-shirts.png"
+        },
+    ],
+    // The foot wear inventory starts here.
+    footwearInventory:[
+        {
+            name: "Leather Shoes",
+            price: 95250,
+            image:"../media/image/Leather shoes.png"
+        },
+        {
+            name:"Women's shoes",
+            price:95000,
+            image:"../media/image/Women's shoes.png"
+        },
+        {
+            name:"Nick Shoes",
+            price:5000,
+            image:"../media/image/nick-shoes.png"
+        },
+    
+        {
+            name:"Shoes",
+            price:35000,
+            image:"../media/image/shoes.png"
+        },
+    
+        {
+            name:"Sneakers",
+            price:125000,
+            image:"..media/image/Sneakers.png"
+        },
+    ]
+    
+}
+export {inventory}
