d23ad8aaf51c00fbec37324edf28cf6b6dbce16b
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-19 09:47:23 +0300

    I have completed the design of the stop clock

--- clock/js/index.js
+++ clock/js/index.js
@@ -38,7 +38,45 @@ btn_start.onclick = ()=>{
                 if(value_sr ==10){
                     let value_sl = +sl.innerHTML
                     value_sl++
-                    sl.innerHTML = value_sl
+                    if(value_sl == 6){
+                        let value_mr = +mr.innerHTML
+                        value_mr++
+                        if(value_mr == 10){
+                            // change the left munite span
+                            let value_ml = +ml.innerHTML // Make it a number with a plus sign
+                            value_ml++
+
+                            // Here is when the value of the left minute clocks six
+                            if(value_ml == 6){
+                                let value_hr = +hr.innerHTML
+                                value_hr++ 
+                                
+                                // When the value of right value of the hour changes.
+                                if(value_hr == 10){
+                                    let value_hl = +hl.innerHTML
+                                    value_hl++
+
+                                    hl.innerText = value_hl
+                                    value_hr = 0
+                                    hr.innerHTML = value_hr
+                                }else{
+                                    hr.innerText = value_hr
+                                }
+                                value_ml = 0
+                                ml.innerHTML = value_ml
+                            }else{
+                                ml.innerText = value_ml
+                            }
+                            value_mr = 0
+                            mr.innerHTML = value_mr
+                        }else{
+                            mr.innerText = value_mr
+                        }
+                        value_sl = 0
+                        sl.innerHTML = value_sl
+                    }else{
+                        sl.innerHTML = value_sl
+                    }
                     value_sr = 0
                     sr.innerText = value_sr
                 }else{
