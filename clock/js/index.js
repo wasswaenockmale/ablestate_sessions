73a910d420522b167cafd5689cfb7e077c3067d9
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-19 08:30:57 +0300

    I have called all the buttons and the areas that are gonna be used

---
+++ clock/js/index.js
@@ -0,0 +1,26 @@
+// Main sections of the stop clock.
+const hour = document.getElementById("hour")
+const minute = document.getElementById("minute")
+const second = document.getElementById("second")
+const micro = document.getElementById("microseconds")
+
+// second section of the stop clock.
+// This includes the left and right
+
+// hour section
+const hl = document.getElementById("hl")
+const hr = document.getElementById("hr")
+
+// minute section
+const ml = document.getElementById("ml")
+const mr = document.getElementById("mr")
+
+// seconds section
+const sl = document.getElementById("sl")
+const sr = document.getElementById("sr")
+
+
+// Now call buttons 
+const btn_start = document.getElementById("start")
+const btn_pause = document.getElementById("pause")
+const btn_stop = document.getElementById("stop")
