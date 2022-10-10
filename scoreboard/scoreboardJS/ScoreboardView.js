d9287f243c037c4076cc27ba6e977f0a7979f1ca
Wasswa Enock Male <enockmale97@gmail.com>
2022-10-10 09:27:08 +0300

    included the click event to the buttons

--- scoreboard/scoreboardJS/ScoreboardView.js
+++ scoreboard/scoreboardJS/ScoreboardView.js
@@ -4,16 +4,16 @@ export default class ScoreboardView{
 
         this.root.innerHTML = `
         <div class="scoreboard">
-            <div class="scoreboard__name scoreboard__name-one" data-for-player="one" >Player One</div>
-            <div class="scoreboard__name scoreboard__name-two" data-for-player="two">Player Two</div>
-            <div class="scoreboard__score">0</div>
-            <div class="scoreboard__score">0</div>
+            <div class="scoreboard__name scoreboard__name-one">${playerOneName}</div>
+            <div class="scoreboard__name scoreboard__name-two">${playerTwoName}</div>
+            <div class="scoreboard__score" data-for-player="one" >0</div>
+            <div class="scoreboard__score" data-for-player="two">0</div>
 
-            <div class="scoreboard__controls">
+            <div class="scoreboard__controls" data-for-player="one" >
                 <button class="scoreboard__control-button">-</button>
                 <button class="scoreboard__control-button">+</button>
             </div>
-            <div class="scoreboard__controls">
+            <div class="scoreboard__controls" data-for-player="two">
                 <button class="scoreboard__control-button">-</button>
                 <button class="scoreboard__control-button">+</button>
             </div>
@@ -22,7 +22,11 @@ export default class ScoreboardView{
 
         this.root.querySelectorAll(".scoreboard__control-button").forEach(element => {
             element.addEventListener("click",()=>{
-                onScoreChange(playerOneName, playerTwoName);
+                const incre = element.textContent == '-' ? "minus":"plus"
+
+                const player = element.closest(".scoreboard__controls").dataset.forPlayer;
+                
+                onScoreChange(player, incre)
             })
         });
     }
