c4d6e81418767d6a5d0e372779d595e8fff8ce36
Wasswa Enock Male <enockmale97@gmail.com>
2022-10-10 09:39:42 +0300

    I have completed the design of the scoreboard

--- scoreboard/main.js
+++ scoreboard/main.js
@@ -7,12 +7,12 @@ let playerTwoName = "Male"
 let playerOneScore = 0;
 let playerTwoScore = 0
 const view = new ScoreboardView(root, {playerOneName, playerTwoName,onScoreChange(player, incre){
-    const difference = incre === "minus" ? -1 : 1
+    const difference = incre === "minus" ? -2 : 2
 
     if(player === "one"){
         playerOneScore = Math.max(difference + playerOneScore, 0)
     }else{
-        playerTwoScore = Math.max(difference + playerTwoScore,0)
+        playerTwoScore = Math.max(difference + playerTwoScore, 0)
     }
 
     view.updateScore(playerOneScore, playerTwoScore)
